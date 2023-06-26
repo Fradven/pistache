3c8070329269ee487e6d61763c64b295f8e9a853
DESKTOP-8KTM3PB\frede <frederic.vanovermeire@gmail.com>
2023-06-26 21:16:44 +0200

    menu done

--- app/components/MenuCard.jsx
+++ app/components/MenuCard.jsx
@@ -1,31 +1,20 @@
 import Image from "next/image";
 
-const MenuCard = () => {
+const MenuCard = ({ title, content, imageURl }) => {
+
+  const createMarkup = () => {
+    return { __html: content };
+  };
+
   return (
     <section className="m-auto px-4 py-2rounded-lg border-2 border-gray-300">
-      <h2 className="text-2xl text-center rounded-md m-3 py-2">Title</h2>
+      <h2 className="text-2xl text-center rounded-md m-3 py-2">{title}</h2>
 
       <div className="w-10/12 m-auto">
-        <Image
-          src={`/images/pictures/menu/american_feast.jpg`}
-          alt="title"
-          width={1200}
-          height={1200}
-        />
+        <Image src={imageURl} alt={title} width={1200} height={1200} />
       </div>
 
-      <article className="p-5">
-        <p>
-          <strong>Starter:</strong> Vietnamese Spring Rolls with Peanut Dipping
-          Sauce
-        </p>
-        <p>
-          <strong>Main Course:</strong> Thai Green Curry with Jasmine Rice
-        </p>
-        <p>
-          <strong>Dessert:</strong> Mango Sticky Rice
-        </p>
-      </article>
+      <article className="p-5" dangerouslySetInnerHTML={createMarkup()} />
     </section>
   );
 };
--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -1,18 +1,39 @@
-import MenuCard from "../components/MenuCard"
+"use client"
+import { useEffect, useState } from "react";
+import MenuCard from "../components/MenuCard";
 import { Raleway } from "next/font/google";
 
 const raleway = Raleway({ weight: ["400"], subsets: ["latin"] });
+const API_ROUTE = "http://localhost:3000/api/getData";
 
-export default async function Menu() {
-    const res = await fetch('../../pages/api/api.php', {
-        method: 'POST',
-        body: JSON.stringify({ action: 'menu' }),
-    })
-    return(
-        <main className={`${raleway.className} grid gap-10 md:grid-cols-fluid my-8 md:mx-6`}>
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-        </main>
-    );
+export default function Menu() {
+  const [menuData, setMenuData] = useState([]);
+
+  useEffect(() => {
+    const fetchMenu = async () => {
+      try {
+        const response = await fetch(API_ROUTE);
+        const data = await response.json();
+        setMenuData(data);
+      } catch (error) {
+        console.error("Error fetching menu:", error);
+      }
+    };
+
+    fetchMenu();
+  }, []);
+
+  useEffect(() => {
+    console.log(menuData);
+  }, [menuData]);
+
+  return (
+    <main className={`${raleway.className} grid gap-10 md:grid-cols-fluid my-8 md:mx-6`}>
+      {menuData.length === 0 ? "Waiting" : menuData.results.map((data) => (
+        <MenuCard key={data.id} title={data.title} content={data.content} imageURl={data.image} />
+      ))}
+    </main>
+  );
 }
+
+
